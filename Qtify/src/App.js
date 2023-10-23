1cddcd335cbf582314e9cfe8ebfe8acee3373aca
PushkarW <wpushkar7@gmail.com>
2023-10-24 00:57:02 +0530

    grid-qtify-takehome

--- Qtify/src/App.js
+++ Qtify/src/App.js
@@ -1,14 +1,35 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import Navbar from "./component/Navbar/Navbar";
 import Hero from "./component/Hero/Hero";
-import Card from "./component/Card/Card";
+import Navbar from "./component/Navbar/Navbar";
+import { fetchTopAlbums } from "./api/api";
+// import Card from "./component/Card/Card";
+import Section from "./component/Section/Section";
 
 function App() {
+  const [topAlbumsData, setTopAlbumsData] = useState([]);
+
+  const generateTopAlbumsData = async () => {
+    try {
+      const data = await fetchTopAlbums();
+      console.log(data);
+      setTopAlbumsData(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    generateTopAlbumsData();
+  }, []);
+
   return (
-    <div className="App">
+    <div>
       <Navbar />
       <Hero />
-      <Card />
+      <div>
+        <Section data={topAlbumsData} type="album" title="Top Albums" />
+      </div>
     </div>
   );
 }
--- Qtify/src/component/Card/Card.jsx
+++ Qtify/src/component/Card/Card.jsx
@@ -1,21 +1,38 @@
 import React from "react";
 import styles from "./Card.module.css";
-import SongImg from "../../assets/songImg.png";
+import { Chip, Tooltip } from "@mui/material";
 
-const Card = () => {
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.card}>
-        <img src={SongImg} alt="album" className={styles.image} />
-        <div className={styles.banner}>
-          <p className={styles.chip}>100M Follows</p>
-        </div>
-      </div>
-      <div className={styles.titleWrapper}>
-        <p>New Bollywood</p>
-      </div>
-    </div>
-  );
+const Card = ({ data, type }) => {
+  const getCard = (type) => {
+    switch (type) {
+      case "album": {
+        const { image, follows, title, songs } = data;
+        return (
+          <Tooltip title={`${songs.length} songs`} placement="top" arrow>
+            <div className={styles.wrapper}>
+              <div className={styles.card}>
+                <img src={image} alt="album" />
+                <div className={styles.banner}>
+                  <Chip
+                    label={`${follows} Follows`}
+                    size="small"
+                    className={styles.chip}
+                  />
+                </div>
+              </div>
+              <div className={styles.titleWrapper}>
+                <p>{title}</p>
+              </div>
+            </div>
+          </Tooltip>
+        );
+      }
+      default:
+        return <></>;
+    }
+  };
+
+  return getCard(type);
 };
 
 export default Card;
